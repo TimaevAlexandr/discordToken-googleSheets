b5f59c3b9b42ca7d10a22a6887a7ba12a5b4fdda
Alexandr <113377990+TimaevAlexandr@users.noreply.github.com>
2023-06-20 19:04:58 +0300

    final release

--- script4.py
+++ script4.py
@@ -1,8 +1,16 @@
+from time import sleep
+
 import requests
 import json
 from datetime import datetime
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+#id ваших таблиц
+#для серверов
+sheet_servers_id = "1QU95zOjy4TSVReB7LRu1-BMOGa6zKxt4wf7l-OQmHtY"
+
+#для аккаунтов
+sheet_accounts_id = "1P-8crhcmSo_0pl_Ii_dniQ40hMGFzcr7Nd9v8dXmrq8"
 
 def get_server_info(TOKEN):
     headers = {'Authorization': f'{TOKEN}'}
@@ -20,6 +28,9 @@ def get_server_info(TOKEN):
         user_id = response.json()["id"]
     else:
         user_id = "error in response"
+
+    #получить больше информации о пользователе
+
     # Создаем файл для сохранения информации
     for server in servers:
         id = server['id']
@@ -45,7 +56,7 @@ def get_server_info(TOKEN):
 
         response = requests.get(f'https://discord.com/api/v9/guilds/{id}', headers=headers)
 
-        print(response.text)
+        # получить всю информацию о сервере ....
 
         if response.status_code != 200:
             role_names = {"error": "error in request"}
@@ -67,19 +78,57 @@ def get_server_info(TOKEN):
 
         string_roles = ""
         for element in roles_of_user:
-            string_roles += (" " + element)
+            string_roles += ("," + element)
 
+        string_roles = string_roles.strip()
 
-        mas.append([id, name, date_formatted, string_roles, user_id])
+        if string_roles.startswith(","):
+            string_roles = string_roles[1::]
+        if string_roles.endswith(","):
+            string_roles = string_roles[:len(string_roles) - 1]
+
+        mas.append([user_id, id, name, date_formatted, string_roles])
 
 
     # Запись нового значения в ячейки
     return mas
 
+def get_account_info(TOKEN):
+    headers = {'Authorization': f'{TOKEN}'}
+    response = requests.get('https://discord.com/api/users/@me', headers=headers)
 
-def main():
+    user_info_dict = response.json()
+
+    user_info_data = []
+    header = ['id', 'username', 'global_name', 'avatar', 'discriminator', 'public_flags', 'flags', 'banner','banner_color', 'accent_color', 'bio', 'locale', 'nsfw_allowed', 'mfa_enabled','premium_type', 'linked_users', 'avatar_decoration', 'email', 'verified', 'phone']
+
+    if response.status_code == 200:
+        #"Загловок для таблицы с данными пользователя"
+
+        for key in header:
+            try:
+                val = user_info_dict[key]
+            except:
+                val = ""
+
+
+
+            if type(val) == list:
+                val = ','.join(val)
+            if val == 'null':
+                val = ""
+
+            user_info_data.append(val)
+
+    else:
+        print("error in response")
+    return user_info_data
+
+    # получить больше информации о пользователе
+def table_with_servers():
     # Авторизация в Google Sheets API
     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+
     # файл с ключом для доступа к api
     token_file_name_for_google_sheets = 's.json'
 
@@ -90,38 +139,105 @@ def main():
     # id вашей таблицы
 
 
-    sheet_id = "1sk3SauxibXScq0FVy3s4KWHYkHHT0_seQrJm6ykbe30"
 
-
-    sheet_url = f'https://docs.google.com/spreadsheets/d/{sheet_id}/edit#gid=0'
+    sheet_url = f'https://docs.google.com/spreadsheets/d/{sheet_servers_id}/edit#gid=0'
     sheet = client.open_by_url(sheet_url).sheet1
     rows = sheet.get_all_values()
-
     rows = rows[1::]
+
+    range_to_clear = 'C2:Z1000'
+    sheet.batch_clear([range_to_clear])
 
     for row in rows:
 
         if type(row) == list:
             row = row[0]
         token = row.strip()
+
+
         if token != "":
-            info = get_server_info(token)
+            print()
+            print(f"обработка токена {token} ..... ")
+            try:
+                info = get_server_info(token)
+                line = 2
+                print(f"Вставка токена {token} .... в таблицу серверов с id {sheet_servers_id} ")
+                try:
+                    for element in info:
+                        element = [token] + element
+                        sheet.append_row(element, table_range=f"C{line}")
+                        line += 1
+                        sleep(1)
+                except:
+                    print("Произошла ошибка, превышено допустимое количество запросов к Google API")
+                    continue
+            except:
+                print("Произошла ошибка, возможно ваш токен не действителен")
         else:
-            print("не корректный токен")
+            #print("пустой токен")
             continue
-        line = 2
-        try:
-            for server_lines in info:
-                sheet.update_acell(f'C{line}', token)
-                sheet.update_acell(f'D{line}', server_lines[4])
-                sheet.update_acell(f'E{line}', server_lines[0])
-                sheet.update_acell(f'F{line}', server_lines[1])
-                sheet.update_acell(f'G{line}', server_lines[2])
-                sheet.update_acell(f'H{line}', server_lines[3])
-                line += 1
-        except:
-            print(row)
-            print("Ошибка")
+
+        sleep(2)
+    #возвращаем список токенов из первой таблицы для заполнения второй
+    mas_tokens = []
+    for row in rows:
+
+        if type(row) == list:
+            row = row[0]
+        token = row.strip()
+        mas_tokens.append(token)
+    return mas_tokens
+
+def table_with_account(tokens):
+    scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+
+    # файл с ключом для доступа к api
+    token_file_name_for_google_sheets = 's.json'
+
+    creds = ServiceAccountCredentials.from_json_keyfile_name(token_file_name_for_google_sheets, scope)
+    client = gspread.authorize(creds)
+
+    # Открытие таблицы по URL
+
+
+    sheet_url = f'https://docs.google.com/spreadsheets/d/{sheet_accounts_id}/edit#gid=0'
+    sheet = client.open_by_url(sheet_url).sheet1
+
+    header = ['token', 'id', 'username', 'global_name', 'avatar', 'discriminator', 'public_flags', 'flags', 'banner', 'banner_color', 'accent_color', 'bio', 'locale', 'nsfw_allowed', 'mfa_enabled', 'premium_type', 'linked_users', 'avatar_decoration', 'email', 'verified', 'phone']
+    range_to_clear = 'A1:Z1000'
+    sheet.batch_clear([range_to_clear])
+    sheet.append_row(header, table_range=f"A{1}")
+
+
+
+    line = 2
+    for token in tokens:
+        if token != "":
+            print()
+            print(f"получение данных аккаунта токена {token} ....")
+            try:
+                user_data = get_account_info(token)
+                try:
+                    print(f"Вставка данных аккаунта токена {token} .... в таблицу аккаунтов с id {sheet_accounts_id} ")
+                    sheet.append_row([token] + user_data, table_range=f"A{line}")
+                    line += 1
+                    sleep(2)
+                except:
+                    print("ошибка, превышено количество запросов к Google API")
+            except:
+                print(f"токен {token} не корректен")
+        else:
             continue
+        sleep(2)
+
 if __name__ == "__main__":
-    main()
+    print("--------------------------")
+    print("Создание таблицы серверов")
+    print("--------------------------")
+
+    tokens = table_with_servers()
+    print("--------------------------")
+    print("Создание таблицы аккаунтов")
+    print("--------------------------")
+
+    table_with_account(tokens)
